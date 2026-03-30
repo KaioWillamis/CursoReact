fb376be7e8052d7012997e8ebbbc0a5d1dc84e38
Kaio Willamis <gsasuke642@gmail.com>
2026-03-30 15:05:19 -0300

    atualizei

--- sesão3/src/App.jsx
+++ sesão3/src/App.jsx
@@ -1,17 +1,10 @@
-import Hero from "./assets/hero.png"
+import ManageData from "./components/ManageData"
 
 function App() {
 
   return (
-    <div className="App">
-        <h1>Avançado em React</h1>
-        
-        <div>
-            <img src="favicon.svg" alt="icone" />
-        </div>
-        <div>
-            <img src={Hero} alt="" />
-        </div>
+    <div >
+      <ManageData/>
     </div>
   )
 }
